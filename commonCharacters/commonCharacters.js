4535f4becbc69d734693ab6cf6a1120745bfffd0
Willson Mock <willson.mock@gmail.com>
2014-12-30 21:43:31 -0500

    Implement commonCharacters.js

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -11,6 +11,61 @@
  */
 
 
-var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+var commonCharacters = function () {
+  var args = Array.prototype.slice.call(arguments);
+  if (args.length < 2) {
+    throw new Error('Not enough string arguments');
+  }
+
+  var result      = '';
+  var firstString = args[0];
+  var arrayOfSets = [];
+  var inter       = {};
+
+  // create an array of sets for all strings in args
+  args.forEach(function (string) {
+    arrayOfSets.push( createSet( string ) );
+  });
+
+  // create master intersect obj
+  inter = intersect.apply(null, arrayOfSets);
+
+  // loop through string1 and if element in inter, push to result
+  firstString.split('').forEach(function (character) {
+    if ( inter[character] ) { result += character; }
+  });
+
+  return result;
+};
+
+var createSet = function (string) {
+  var result = {};
+  var arr    = string.split('');
+
+  arr.forEach(function (character) {
+    result[character] = true;
+  });
+
+  return result;
+};
+
+var intersect = function () { // assume each argument is an object
+  var args  = Array.prototype.slice.call(arguments);
+
+  var inner = function (first, arr) {
+    var result, prop;
+    // base case
+    if ( arr.length === 0 ) { return first; }
+
+    result = {};
+    for ( prop in first ) {
+      if ( arr[0][prop] ) {
+        result[prop] = true;
+      }
+    }
+
+    return inner( result, arr.slice(1) );
+  };
+
+  return inner( args[0], args.slice(1) );
 };
