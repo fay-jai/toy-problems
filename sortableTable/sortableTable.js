e8130fe983813145aeb3e2363bbe4f307babc3bf
Willson Mock <willson.mock@gmail.com>
2015-01-02 00:51:28 -0500

    Work in progress for sortableTable.js

--- sortableTable/sortableTable.js
+++ sortableTable/sortableTable.js
@@ -9,7 +9,44 @@
  **/
 
 $(function () {
-  // TODO: your code here!
+  var convert = function (type, value) {
+    switch (type) {
+      case 0:
+        return '' + value;
+      case 1:
+        return parseInt(value, 10);
+      case 2:
+        return parseFloat(value);
+      case 3:
+        return new Date(value);
+    }
+  };
 
+  var sort = function (array) {
+    return Array.prototype.sort.call(array, function (a, b) {
+      return b[0] - a[0];
+    });
+  };
+
+  var $tr = $('tr');
+
+  $('th').each(function (thIdx, thElement) {
+    $(thElement).on('click', function () {
+      var result = [];
+      var order  = [];
+
+      for (var i = 1; i < $tr.length; i += 1) {
+        result.push( [ convert(thIdx, $tr[i].children[thIdx].innerHTML), i] );
+      }
+
+      sort(result);
+
+      result.forEach(function (array) {
+        order.push( array[1] );
+      });
+
+      console.log( order );
+    });
+  });
 });
 
