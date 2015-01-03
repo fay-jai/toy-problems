9ba5a0870a3ee31a17ab9c2664a3bc8badb46d8a
Willson Mock <willson.mock@gmail.com>
2015-01-03 13:22:05 -0500

    Implement longestPalindrome.js

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -7,7 +7,27 @@
  */
 
 var longestPalindrome = function (string) {
-  // Your code here
+  var result = '';
+  var len = string.length;
+  var i, j, word;
+
+  for (i = 0; i < len; i += 1) {
+    for (j = i + 1; j < len + 1; j += 1) {
+      word = string.slice(i, j);
+
+      if ( isPalindrome( word ) && word.length > result.length) {
+        result = word;
+      }
+    }
+  }
+
+  return result;
 };
 
+var isPalindrome = function (string) {
+  var len = string.length;
 
+  if ( len < 2 ) return true;
+  if ( string[0] !== string[len - 1] ) return false;
+  return isPalindrome( string.slice(1, len - 1) );
+};
