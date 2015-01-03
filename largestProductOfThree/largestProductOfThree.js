61eb59fcd865bd548e7b72542ae927a1bc5182fd
Willson Mock <willson.mock@gmail.com>
2015-01-03 13:03:38 -0500

    Implement largestProductOfThree.js using naive approach

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -4,6 +4,25 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-var largestProductOfThree = function(array) {
-  // TODO: everything
+var largestProductOfThree = function (array) {
+  // Time Complexity: O(n^3)
+
+  var result = [];
+  var len    = array.length;
+  var first, second, third;
+  var i, j, k;
+
+  for (i = 0; i < len; i += 1) {
+    first = array[i];
+    for (j = i + 1; j < len; j += 1) {
+      second = array[j];
+      for (k = j + 1; k < len; k += 1) {
+        third = array[k];
+
+        result.push( first * second * third );
+      }
+    }
+  }
+
+  return Math.max.apply(null, result);
 };
