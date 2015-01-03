f1cc5869284a2c7e91c6193df0d3df0e4386cf50
Willson Mock <willson.mock@gmail.com>
2015-01-03 13:04:28 -0500

    Implement largestProductOfThree.js after sorting - O(n * log(n))

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -26,3 +26,31 @@ var largestProductOfThree = function (array) {
 
   return Math.max.apply(null, result);
 };
+
+var largestProductOfThree = function (array) {
+  // Time Complexity: O(n * log(n))
+
+  if ( !Array.isArray(array) ) throw new Error('Must provide an Array of numbers');
+  if ( array.length < 3 ) throw new Error('Array must have at least 3 numbers');
+
+  /*
+   * Array has at least 3 elements
+  */
+
+  var len = array.length;
+  var sortedArray, optionA, optionB;
+
+  // sort the array => O(n * log(n))
+  sortedArray = array.sort(function (a, b) { return a - b; });
+
+  /*
+   * The largest product of three numbers can be either:
+   * 1) the two most negative numbers and the most positive number (option A)
+   * 2) the three most positive numbers (option B)
+  */
+
+  optionA = array[0] * array[1] * array[len - 1];
+  optionB = array[len - 3] * array[len - 2] * array[len - 1];
+
+  return Math.max( optionA , optionB );
+};
