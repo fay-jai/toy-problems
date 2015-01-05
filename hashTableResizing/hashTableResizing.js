0c30c1847723f61c30150a2c4b27d9d776fabf9e
Willson Mock <willson.mock@gmail.com>
2015-01-05 10:14:17 -0500

    Implement hashTable in hashTableResizing.js

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -7,21 +7,69 @@
  * Resize by half whenever utilization drops below 1/4.
  */
 
-var makeHashTable = function(){
-  var result = {};
-  var storage = [];
+var makeHashTable = function () {
+  var result       = {};
+  var storage      = [];
   var storageLimit = 4;
-  var size = 0;
-  result.insert = function(/*...*/){
-    // TODO: implement `insert`
+  var size         = 0;
+
+  result.insert = function (key, value) {
+    var idx    = getIndexBelowMaxForKey(key, storageLimit);
+    var bucket = storage[idx] || [];
+    var len    = bucket.length;
+    var i;
+
+    // determine whether the key, value pair already exists as an element in bucket
+    for ( i = 0; i < len; i += 1 ) {
+      // if the same key already exists, replace its old value with the new one
+      if ( bucket[i][0] === key ) {
+        bucket[i][1] = value;
+        return;
+      }
+    }
+
+    // at this point, the key, value pair doesn't exist in the bucket
+    bucket.push( [key, value] );
+    storage[idx] = bucket;
+
+    // increase size
+    size += 1;
   };
 
-  result.retrieve = function(/*...*/){
-    // TODO: implement `retrieve`
+  result.retrieve = function (key) {
+    var idx    = getIndexBelowMaxForKey(key, storageLimit);
+    var bucket = storage[idx] || [];
+    var len    = bucket.length;
+    var i;
+
+    for ( i = 0; i < len; i += 1 ) {
+      if ( bucket[i][0] === key ) return bucket[i][1];
+    }
   };
 
-  result.remove = function(/*...*/){
-    // TODO: implement `remove`
+  result.remove = function (key) {
+    var idx    = getIndexBelowMaxForKey(key, storageLimit);
+    var bucket = storage[idx] || [];
+    var len    = bucket.length;
+    var value, i;
+
+    for ( i = 0; i < len; i += 1 ) {
+      if ( bucket[i][0] === key ) {
+        value = bucket.splice(i, 1);
+
+        size -= 1;
+
+        return value[0];
+      }
+    }
+  };
+
+  result.adjustStorageLimit = function () {
+    // for now, focus on case where you're doubling the storage limit
+
+    // determine what the new storage limit is
+    // create a new storage array
+    // for each item in the current storage array
   };
 
   return result;
@@ -30,7 +78,7 @@ var makeHashTable = function(){
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between
 // 0 and max - 1
-var getIndexBelowMaxForKey = function(str, max){
+var getIndexBelowMaxForKey = function (str, max) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
     hash = (hash<<5) + hash + str.charCodeAt(i);
