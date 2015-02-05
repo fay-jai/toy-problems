1519675470c12ef2eeb2dfb6a87479d0239ae452
Willson Mock <willson.mock@gmail.com>
2015-02-05 14:30:08 -0800

    Implement makeChange with comments - key-concepts.js

--- key-concepts.js
+++ key-concepts.js
@@ -125,6 +125,9 @@ var combinations = function (array) {
  * Given a sorted array, perform binary search
 */
 
+/*
+ * Key Concepts: array indices, recursion, O(log n)
+*/
 var binarySearch = function (sortedArray, value) {
   var len   = sortedArray.length;
   var inner = function (start, end) {
@@ -187,36 +190,61 @@ var isSubsetOf = function (base, input) {
 };
 
 /*
-
-In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
-
-1p piece
-2p piece
-5p piece
-10p piece
-20p piece
-50p piece
-1 euro (100p)
-2 euro (200p)
-
-It is possible to make £2 in the following way:
-
-1 * £1 + 1 * 50p + 2 * 20p + 1 * 5p + 1 * 2p + 3 * 1p
-How many different ways can £2 be made using any number of coins?
-
-example usage of `makeChange`:
-
-// aka, there's only one way to make 1p. that's with a single 1p piece
-makeChange(1) === 1
-// aka, there's only two ways to make 2p. that's with two, 1p pieces or with a single 2p piece
-makeChange(2) === 2
+ * In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+ *
+ * 1p piece
+ * 2p piece
+ * 5p piece
+ * 10p piece
+ * 20p piece
+ * 50p piece
+ * 1 euro (100p)
+ * 2 euro (200p)
+ *
+ * It is possible to make £2 in the following way:
+ *
+ * 1 * £1 + 1 * 50p + 2 * 20p + 1 * 5p + 1 * 2p + 3 * 1p
+ * How many different ways can £2 be made using any number of coins?
+ *
+ * Example usage of `makeChange`:
+ *
+ * There's only one way to make 1p. that's with a single 1p piece
+ * makeChange(1) === 1
+ *
+ * There's only two ways to make 2p. that's with two, 1p pieces or with a single 2p piece
+ * makeChange(2) === 2
 */
 
+/*
+ * Key Concepts: using reduce for recursion, recursion, tail recursion, creating placeholder functions
+*/
 var makeChange = function (amt) {
+  var inner = function (amt, coins) {
+    // establish base cases
+    if (amt  <  0) return 0;
+    if (amt === 0) return 1;
 
+    // recursive case
+    /*
+     * For the given amt, find the available coins that can be used.
+     * For each of those coins, return the accumulated sum of inner with the
+     * amt reduced by the coin used and available coins equal to coin amt.
+    */
+    return coins.reduce(function (total, eachCoin) {
+      return total + inner(amt - eachCoin, availableCoins(eachCoin));
+    }, 0)
+  };
+
+  return inner(amt, availableCoins(amt));
 };
 
-/**
+var availableCoins = function (amt) {
+  return [200, 100, 50, 20, 10, 5, 2, 1].filter(function (value) {
+    return amt >= value;
+  });
+};
+
+/*
  * Make an eventing system mix-in that adds .trigger() and .on() to any input
  * object.
  *
@@ -259,7 +287,7 @@ var mixEvents = function (obj) {
   return obj;
 };
 
- /*
+/*
  * function bind():
  *
  * example 1:
